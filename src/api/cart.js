97a4f688541643a5b235eaf30418a13620917ca2
unknown <robert_nokkvi@hotmail.com>
2022-02-27 18:52:38 +0000

    order og cart get

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,3 +1,32 @@
 import express from 'express';
+import { query } from '../db.js';
+import { catchErrors } from '../utils/catch-errors.js';
+import { validationCheck } from '../validation/helper.js';
 
 export const router = express.Router();
+
+async function getCartId(req,res) {
+
+  const { cartid } = req.params;
+  console.log(cartid)
+
+  const cartQuery = `
+  SELECT
+    *
+  FROM
+    basket_items
+  WHERE
+    uid=$1
+  `;
+
+  const cart = await query(
+    cartQuery,
+    [cartid],
+  );
+
+  return res.json(cart.rows);
+}
+
+router.get('/:cartid',
+            validationCheck,
+            catchErrors(getCartId))
--- src/api/orders.js
+++ src/api/orders.js
@@ -1,3 +1,39 @@
 import express from 'express';
+import { join } from 'path';
+import { requireAdmin, requireAuthentication } from '../auth/passport.js';
+import { pagingQuerystringValidator } from '../validation/validators.js';
+import { pagedQuery } from '../db.js';
+import { validationCheck } from '../validation/helper.js';
+import { catchErrors } from '../utils/catch-errors.js';
+import { addPageMetadata } from '../utils/addPageMetadata.js';
 
 export const router = express.Router();
+
+async function listOrders(req,res) {
+  const { offset = 0, limit = 10 } = req.query;
+
+  const orderQuery = `
+    SELECT
+      uid, created, name
+    FROM
+      orders
+    ORDER BY created DESC
+    `;
+
+  const orders = await pagedQuery(orderQuery, [], { offset, limit });
+
+  const ordersWithPage = addPageMetadata(
+    orders,
+    join('/orders', req.path),
+    { offset, limit, length: orders.items.length },
+  );
+
+  return res.json(ordersWithPage);
+}
+
+router.get('/',
+          requireAuthentication,
+          requireAdmin,
+          pagingQuerystringValidator,
+          validationCheck,
+          catchErrors(listOrders))
